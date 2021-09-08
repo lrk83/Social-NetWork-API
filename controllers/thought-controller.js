64f9c390563f27df05dd0dd2e52c348fbad4a20e
Lukas Keel <lrk83@cornell.edu>
2021-09-08 13:50:30 -0400

    update and delete thought routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -40,35 +40,39 @@ const thoughtController = {
     },
 
     getThoughtById(req,res){
-        Thought.findOne({ _id: params.id })
-            .then(dbThoughtData => res.json(dbUserData))
+        Thought.findOne({ _id: req.params.id })
+            .then(dbThoughtData => res.json(dbThoughtData))
             .catch(err => {
                 console.log(err);
                 res.sendStatus(400);
             });
     },
 
-    updateThought(){
-        
+    updateThought(req,res){
+        Thought.findOneAndUpdate({ _id: req.params.id},req.body,{ new: true, runValidators: true })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                  res.status(404).json({ message: 'No thought found with this id!' });
+                  return;
+                }
+                res.json(dbThoughtData);
+              })
+              .catch(err => res.json(err))
     },
 
-    removeThought(){
-        Thought.findOneAndDelete({ _id: params.thoughtId })
+    removeThought(req,res){
+        Thought.findOneAndDelete({ _id: req.params.id })
             .then(deletedThought => {
                 if (!deletedThought) {
                 return res.status(404).json({ message: 'No thought with this id!' });
             }
             return User.findOneAndUpdate(
-                { _id: params.userId },
-                { $pull: { thoughts: params.thoughtId } },
+                { thoughts: req.params.id },
+                { $pull: { thoughts: req.params.id} },
                 { new: true }
             );
         })
         .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id!' });
-                return;
-            }
             res.json(dbUserData);
         })
         .catch(err => res.json(err));
